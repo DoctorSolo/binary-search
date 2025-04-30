39f2ffe84ed9af7d7ce81c39f395f9e9bd8c02bf
Miguel Eduardo <dasilvamigueleduardo@gmail.com>
2025-04-30 14:35:09 -0300

    feat

--- BinarySearch.py
+++ BinarySearch.py
@@ -7,10 +7,8 @@ class BinarySearch:
         start = self.arr[0]
         end = len(self.arr) - 1
         
-        middle_search = lambda start, end: ((start + end) // 2)
-        
         while(start <= end):
-            middle = middle_search(start, end) 
+            middle = ((start + end) // 2)
             
             if self.arr[middle] == search:
                 return middle
@@ -21,3 +19,15 @@ class BinarySearch:
         return -1
     
     
+    def Add(self, number:int) -> None:
+        
+        self.arr.append(-1)
+        arr_lenght = len(self.arr)
+        
+        for i in range(arr_lenght - 1):
+            if (self.arr[i + 1] > number):
+                self.arr[i+1: arr_lenght-1] = range(self.arr[i], arr_lenght)
+                self.arr[i] = number
+                break
+        self.arr[arr_lenght] = number
+        
